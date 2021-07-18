9e36a1fad878b7ce368d4bf8585fee51ede1eea5
aryanblouria <57865255+aryanblouria@users.noreply.github.com>
2021-07-19 03:58:15 +0530

    Add files via upload

---
+++ #01 - Favourite Number/favourite_number.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int fav_num;
+    cout<<"Enter your favorite number: ";
+    cin>>fav_num;
+    cout<<"Wow! "<<fav_num<<" is my favorite number too.";
+    return 0;
+}
