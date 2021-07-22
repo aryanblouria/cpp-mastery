49013cd100f608d3a29300dc6c6f2fe2a6332e86
aryanblouria <57865255+aryanblouria@users.noreply.github.com>
2021-07-23 03:13:30 +0530

    Add files via upload

---
+++ #06 - Substitution Cipher/substitution_cipher.cpp
@@ -0,0 +1,36 @@
+#include<iostream>
+#include<string>
+using namespace std;
+
+int main()
+{
+    string alphabets = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789 ";
+    string key = "ZYXWVUTSRQPONMLKJIHGFEDCBAzyxwvutsrqponmlkjihgfedcba9876543210 ";
+    string message, encrypted_message, decrypted_message;
+
+    int e_index = 0, d_index = 0;
+
+    cout<<"Walmart Encryption Service"<<endl;
+    cout<<"=========================="<<endl<<endl;
+
+    cout<<"Enter the message: ";
+    getline(cin, message);
+
+    encrypted_message = message;
+    for (size_t i = 0; i < message.length(); i++)
+    {
+        e_index = alphabets.find(message[i]);
+        encrypted_message[i] = key[e_index];
+    }
+    cout<<"Encrypted message: "<<encrypted_message<<endl;
+
+    decrypted_message = encrypted_message;
+    for (size_t i = 0; i < encrypted_message.length(); i++)
+    {
+        d_index = key.find(encrypted_message[i]);
+        decrypted_message[i] = alphabets[d_index];
+    }
+    cout<<"Decrypted message: "<<decrypted_message<<endl;
+
+    return 0;
+}
